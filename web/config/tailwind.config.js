14a3bb6c149a501da03e326a699b294d171e8d1d
Ari Mendelow <ari@mendelow.me>
2023-10-07 17:17:11 -0700

    Start to build out color system

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+        neutral: colors.gray,
+        accent: colors.teal,
+      },
+    },
   },
   plugins: [],
 }
-
