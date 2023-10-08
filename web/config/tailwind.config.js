92bc2fd8dda140aa4375ffd002c8cb3b0255813f
Ari Mendelow <ari@mendelow.me>
2023-10-07 19:08:01 -0700

    Start work on dark mode

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
