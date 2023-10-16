831a73da43c20c9e5c18b59751ad47838bcbb2b1
Ari Mendelow <ari@mendelow.me>
2023-10-16 10:53:30 -0700

    Start to build overlay

---
+++ web/src/components/Overlay/Overlay.tsx
@@ -0,0 +1,10 @@
+const Overlay = () => {
+  return (
+    <div>
+      <h2>{'Overlay'}</h2>
+      <p>{'Find me in ./web/src/components/Overlay/Overlay.tsx'}</p>
+    </div>
+  )
+}
+
+export default Overlay
