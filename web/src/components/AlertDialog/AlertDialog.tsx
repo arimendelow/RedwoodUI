4ed299845ea7819028af5fafd64cbfa0276276c2
Ari Mendelow <ari@mendelow.me>
2023-10-28 14:12:10 -0500

    Start to add alert dialog

---
+++ web/src/components/AlertDialog/AlertDialog.tsx
@@ -0,0 +1,10 @@
+const AlertDialog = () => {
+  return (
+    <div>
+      <h2>{'AlertDialog'}</h2>
+      <p>{'Find me in ./web/src/components/AlertDialog/AlertDialog.tsx'}</p>
+    </div>
+  )
+}
+
+export default AlertDialog
