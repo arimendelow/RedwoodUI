7d22915e5b85792ab93b18e27648cb751f9f3ba6
Ari Mendelow <ari@mendelow.me>
2023-10-29 15:24:44 -0500

    Fix story

--- web/src/components/Accordion/Accordion.stories.tsx
+++ web/src/components/Accordion/Accordion.stories.tsx
@@ -12,14 +12,11 @@
 
 import type { Meta, StoryObj } from '@storybook/react'
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from './Accordion'
-import { RedwoodJSLogo } from '../logos/RedwoodJSLogo'
 import { ChildrenPlaceholder } from 'src/lib/StorybookUtils'
+
+import { RedwoodJSLogo } from '../logos/RedwoodJSLogo'
+
+import { Accordion } from './Accordion'
 
 const meta: Meta<typeof Accordion> = {
   component: Accordion,
@@ -37,8 +34,8 @@ const meta: Meta<typeof Accordion> = {
       table: {
         disable: true,
       },
-    }
-  }
+    },
+  },
 }
 
 export default meta
@@ -49,23 +46,43 @@ export const Primary: Story = {
   args: {
     type: 'single',
   },
-  render: ({type}) => (
-    <Accordion type={type} collapsible sections={[{
-      trigger: "Is it accessible?",
-      content: "Yes. It adheres to the WAI-ARIA design pattern, as it's built with Radix UI.",
-    },
-    {
-      trigger: "Is it styled?",
-      content: "Yes! It matches all other components, including built-in dark mode support 😉",
-    },
-    {
-      trigger: "Is it animated?",
-      content: "Yes, with CSS - but you can disable it if you prefer. If you're interested in instead animating with Framer Motion, refer to the docs for why we didn't do that.",
-    },
-    {
-      trigger: <div>Can I use a component as the trigger and/or content? <RedwoodJSLogo className='inline h-5 w-5'/></div>,
-      content: <div>Yes! You can use either a string or component for either of these.<div className='h-32'><ChildrenPlaceholder /></div></div>
-    },
-  ]}/>
+  render: ({ type }) => (
+    <Accordion
+      type={type}
+      collapsible
+      sections={[
+        {
+          trigger: 'Is it accessible?',
+          content:
+            "Yes. It adheres to the WAI-ARIA design pattern, as it's built with Radix UI.",
+        },
+        {
+          trigger: 'Is it styled?',
+          content:
+            'Yes! It matches all other components, including built-in dark mode support 😉',
+        },
+        {
+          trigger: 'Is it animated?',
+          content:
+            "Yes, with CSS - but you can disable it if you prefer. If you're interested in instead animating with Framer Motion, refer to the docs for why we didn't do that.",
+        },
+        {
+          trigger: (
+            <div className="text-left">
+              Can I use a component as the trigger and/or content?{' '}
+              <RedwoodJSLogo className="inline h-5 w-5" />
+            </div>
+          ),
+          content: (
+            <div>
+              Yes! You can use either a string or component for either of these.
+              <div className="h-32">
+                <ChildrenPlaceholder />
+              </div>
+            </div>
+          ),
+        },
+      ]}
+    />
   ),
 }
