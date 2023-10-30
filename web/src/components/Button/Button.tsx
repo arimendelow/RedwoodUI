034261402e11885541b33d0b23b71042d1f7cc76
Ari Mendelow <ari@mendelow.me>
2023-10-30 16:20:36 -0500

    Add active states

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -6,30 +6,35 @@ import { cn } from 'src/lib/utils'
 const buttonColorTreatments = {
   primary: [
     'bg-primary-700 dark:bg-primary-500',
+    'active:bg-primary-800 dark:active:bg-primary-600',
     'text-light',
     'hover:bg-primary-700/90 dark:hover:bg-primary-500/90',
     'focus-ring',
   ],
   secondary: [
     'bg-dark/10 dark:bg-light/10',
+    'active:bg-dark/30 dark:active:bg-light/30',
     'hover:bg-dark/20 dark:hover:bg-light/20',
     'text-dark dark:text-light',
     'focus-ring',
   ],
   soft: [
     'bg-primary-50 dark:bg-primary-900/50',
+    'active:bg-primary-200 dark:active:bg-primary-700/80',
     'text-primary-600 dark:text-light/90',
     'hover:bg-primary-100 dark:hover:bg-primary-900/80',
     'focus-ring',
   ],
   minimal: [
     'bg-transparent',
+    'active:bg-primary-100 dark:active:bg-primary-700/80',
     'text-primary-600 dark:text-light/90',
     'hover:bg-primary-50 dark:hover:bg-primary-900/80',
     'focus-ring',
   ],
   danger: [
     'bg-red-600 dark:bg-red-700',
+    'active:bg-red-800 dark:active:bg-red-900',
     'text-light',
     'hover:bg-red-700 dark:hover:bg-red-800',
     'focus-ring',
@@ -46,7 +51,7 @@ const buttonSizeTreatments = {
 }
 
 const buttonVariants = cva(
-  'rounded-default inline-flex items-center justify-center font-semibold transition-colors duration-200 disabled:disabled-classes',
+  'rounded-default inline-flex items-center justify-center font-semibold transition-colors duration-200 disabled:disabled-classes cursor-pointer',
   {
     variants: {
       colorTreatment: buttonColorTreatments,
