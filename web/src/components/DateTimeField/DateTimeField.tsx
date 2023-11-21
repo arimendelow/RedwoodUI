04535b0085cc493a0011cd0c406c5501dead5b0a
Ari Mendelow <ari@mendelow.me>
2023-11-21 13:11:49 -0500

    Undo switch

--- web/src/components/DateTimeField/DateTimeField.tsx
+++ web/src/components/DateTimeField/DateTimeField.tsx
@@ -1,9 +1,37 @@
-const DateTimeField = () => {
+import DatePickerPrimitive from 'react-datepicker'
+
+type DatePickerPrimitivePropsType = React.ComponentProps<
+  typeof DatePickerPrimitive
+>
+
+interface IDateTimeFieldProps
+  extends Omit<
+    DatePickerPrimitivePropsType,
+    'selected' | 'onChange' | 'startDate' | 'endDate'
+  > {}
+
+const DateTimeField = ({ ...props }: IDateTimeFieldProps) => {
+  const [startDate, setStartDate] = React.useState<Date | null>(null)
+  const [endDate, setEndDate] = React.useState<Date | null>(null)
+  const onDateSelectionChange: DatePickerPrimitivePropsType['onChange'] = (
+    dateSelection
+  ) => {
+    if (Array.isArray(dateSelection)) {
+      const [start, end] = dateSelection
+      setStartDate(start)
+      setEndDate(end)
+    } else {
+      setStartDate(dateSelection)
+    }
+  }
   return (
-    <div>
-      <h2>{'DateTimeField'}</h2>
-      <p>{'Find me in ./web/src/components/DateTimeField/DateTimeField.tsx'}</p>
-    </div>
+    <DatePickerPrimitive
+      selected={startDate}
+      onChange={onDateSelectionChange}
+      startDate={startDate}
+      endDate={endDate}
+      {...props}
+    />
   )
 }
 
