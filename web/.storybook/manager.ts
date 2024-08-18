91a703132100298dc4ecd1f3affa8203eabd1eb1
ari <ari@mendelow.me>
2024-08-18 15:32:05 -0700

    Add theme; fix versions

---
+++ web/.storybook/manager.ts
@@ -0,0 +1,12 @@
+import { addons } from '@storybook/manager-api'
+import { create } from '@storybook/theming'
+
+addons.setConfig({
+  theme: create({
+    base: 'dark',
+
+    brandImage: 'https://redwoodjs.com/images/logo--dark.svg',
+    brandTitle: 'RedwoodJS Components',
+    brandUrl: 'https://redwoodjs.com/',
+  }),
+})
